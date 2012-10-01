17fc56fc6e3aaed82f5cfd382c4db8701de29be7
Dahua Lin <lindahua@gmail.com>
2012-09-30 19:09:53 -0500

    add sample_wor (tested)

---
+++ common/sample_wor.m
@@ -0,0 +1,57 @@
+function x = sample_wor(m, n)
+%SAMPLE_WOR Random sampling without replacement
+%
+%   x = SAMPLE_WOR(m, n)
+%
+%       Draws n integers from 1:m without replacement. 
+%       
+%       Here, both m and n are positive integers, with n <= m.
+%
+%       The size of x will be [n 1]
+%
+
+%% argument checking
+
+if ~(isreal(m) && isscalar(m) && m > 0)
+    error('sample_wor:invalidarg', 'm should be a positive scalar.');
+end
+
+if ~(isreal(n) && isscalar(n) && n > 0 && n <= m)
+    error('sample_wor:invalidarg', ...
+        'n should be a positive scalar with n <= m.');
+end
+
+%% main
+
+if n == 1
+    x = randi(m, 1);
+    
+elseif n == 2
+    x = randi(m, [2 1]);
+    while x(2) == x(1)
+        x(2) = randi(m, 1);
+    end
+    
+else
+    if n * 5 < m
+        b = false(m, 1);
+        b(randi(m, [n 1])) = 1;
+        
+        s = sum(b);
+        while s < n
+            b(randi(m, [n - s, 1])) = 1;
+            s = sum(b);
+        end
+        
+        x = find(b);
+        x = x(randperm(n));
+        
+    else
+        x = randperm(m);
+        if n < m
+            x = x(1:n);
+        end
+    end
+end
+
+
