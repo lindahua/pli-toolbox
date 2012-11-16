77a86e3212264e07aec161b0c9c05737a77e94dc
Dahua Lin <lindahua@gmail.com>
2012-11-16 09:57:53 -0600

    completed pli_kernel

--- metrics/pli_kernel.m
+++ metrics/pli_kernel.m
@@ -1,30 +1,78 @@
 function K = pli_kernel(name, X, Y, varargin)
 %PLI_KERNEL Kernel evaluation
 %
-%   K = PLI_KERNEL('poly', X, Y, d);
+%   K = PLI_KERNEL(kername, X, [], ...);
+%   K = PLI_KERNEL(kername, X, Y, ...);
+%
+%       This is the general syntax of using this function.
+%
+%       Evaluates the specified kernel function between columns in X and
+%       those in Y. Suppose X and Y respectively have m and n columns,
+%       then the resultant matrix K will be a matrix of size m-by-n.
+%
+%       Setting Y to empty means to computer pairwise kernel values 
+%       between columns in X. 
+%
+%
+%   Specific kernels supported by this function and their usage:
+%
+%   K = PLI_KERNEL('lin', X, Y);
+%
+%       Linear kernel: k(x, y) = x' * y;
+%
+%
+%   K = PLI_KERNEL('poly', X, Y, d, a);
 %       
-%       Evaluates Polynomial kernel between the columns in X and those 
-%       in Y. d is the degree of the polynomial.
+%       Polynomial kernel: k(x, y; d, a) = (x' * y + a)^d
+%       Default params: d = 2, a = 1.
+%
 %
 %   K = PLI_KERNEL('gauss', X, Y, sigma);
+%   K = PLI_KERNEL('rbf', X, Y, sigma);
 %
-%       Evaluates Gaussian kernel between the columns in X and those in Y.
-%       sigma is the band-width of the kernel.
+%       Gaussian RBG kernel: 
+%           k(x, y; sigma) = exp(- (1/2) * (x - y)^2 / sigma^2)
+%
+%       Default params: sigma = 1.
+%
+%
+%   K = PLI_KERNEL('tanh', X, Y, a, b);
+%   K = PLI_KERNEL('mlp', X, Y, a, b);
+%
+%       Multi-layer perceptron kernel:
+%           K(x, y; a, b) = tanh(a * x' * y + b);
+%       Default params: a = 1, b = -1.           
+%
+%
+%   K = PLI_KERNEL(kfun, X, Y, ...);
+%
+%       User-defined kernel function. Here, kfun is a function-handle.
+%
+%       kfun(X, Y, ...) should return an m-by-n matrix, suppose X and Y
+%       respectively have m and n columns.
 %
 
 %% argument checking
 
-if ~ischar(name)
+if ischar(name)
+    use_preset = 1;
+elseif isa(name, 'function_handle')
+    use_preset = 0;
+    kfun = name;    
+else    
     error('pli_kernel:invalidarg', ...
-        'The first argument should be a kernel name.');
+        'The first argument should be a kernel name or a function handle.');
 end
 
 if ~(isfloat(X) && isreal(X) && ismatrix(X))
     error('pli_kernel:invalidarg', 'X should be a real matrix.');
 end
 
-if ~isempty(Y)
+if nargin < 3
+    Y = [];
+end
 
+if ~isempty(Y)
     if ~(isfloat(Y) && isreal(Y) && ismatrix(Y))
         error('pli_kernel:invalidarg', 'Y should be a real matrix.');
     end
@@ -37,21 +85,36 @@ end
 
 %% main
 
-switch lower(name)
-    case 'poly'
-        K = poly_kernel(X, Y, varargin{:});
-        
-    case 'gauss'
-        K = gauss_kernel(X, Y, varargin{:});
-        
-    otherwise
-        error('pli_kernel:invalidarg', ...
-            'Unknown kernel name %s', name);
+if use_preset
+    switch lower(name)
+        case 'lin'
+            K = lin_kernel(X, Y, varargin{:});
+            
+        case 'poly'
+            K = poly_kernel(X, Y, varargin{:});
+            
+        case {'rbf', 'gauss'}
+            K = rbf_kernel(X, Y, varargin{:});
+            
+        case {'mlp', 'tanh'}
+            K = mlp_kernel(X, Y, varargin{:});
+            
+        otherwise
+            error('pli_kernel:invalidarg', ...
+                'Unknown kernel name %s', name);
+    end
+else
+    if isempty(Y)
+        Y = X;
+    end
+    
+    K = kfun(X, Y, varargin{:}); 
 end
 
-%% Core
 
-function K = poly_kernel(X, Y, d)
+%% kernel functions
+
+function K = lin_kernel(X, Y)
 
 if isempty(Y)
     K = X' * X;
@@ -59,17 +122,57 @@ else
     K = X' * Y;
 end
 
-K = (K + 1).^d;
-    
+function K = poly_kernel(X, Y, d, a)
 
-function K = gauss_kernel(X, Y, sigma)
-
-if isempty(X)
-    D = pli_pw_euclidean(X, [], 'sq');
-else
-    D = pli_pw_euclidean(X, Y, 'sq');
+if nargin < 3
+    d = 2;
 end
 
-K = exp((-1 / sigma^2) * D);
+if nargin < 4
+    a = 1;
+end
 
+if isempty(Y)
+    K = X' * X;
+else
+    K = X' * Y;
+end
+
+if a ~= 0
+    K = K + a;
+end
+
+if d == 2
+    K = K .* K;
+elseif d ~= 1
+    K = K .^ d;
+end
+    
+
+function K = rbf_kernel(X, Y, sigma)
+
+if nargin < 3
+    sigma = 1;
+end
+
+D = pli_pw_euclidean(X, Y, 'sq');
+K = exp((-0.5 / sigma^2) * D);
+
+
+function K = mlp_kernel(X, Y, a, b)
+
+if nargin < 3; a = 1; end
+if nargin < 4; b = 1; end
+
+if isempty(Y)
+    K = X' * X;
+else
+    K = X' * Y;
+end
+
+if a ~= 1
+    K = K * a;
+end
+
+K = tanh(K + b);
 
